5bcc59c0da5dea3407c486ec09e24cddc1885c14
Ivan Dorofenko <linik.katon@gmail.com>
2024-06-17 10:36:22 +0400

    Second Task

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 
 #Python Control Work
+
+section_a = [2, 0, 509, 23]
+section_b = [45, 0, 14, 8, 1]
+section_c = []
+
+section_c.append(''.join(map(str, sorted(section_a))))
+section_c.append(''.join(map(str, sorted(section_a, reverse=True))))
+section_c.append(''.join(map(str, sorted(section_b))))
+section_c.append(''.join(map(str, sorted(section_b, reverse=True))))
+
+print(*section_c)
