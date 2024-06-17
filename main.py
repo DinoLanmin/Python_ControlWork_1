3c0db51775782eac4d56775c405c72443f6a7f77
Ivan Dorofenko <linik.katon@gmail.com>
2024-06-17 10:26:46 +0400

    First Task

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 
 #Python Control Work
+
+library_a = ['1', '2', '3', '4', '5']
+library_b = ['a', 'b', 'c', 'd', 'e']
+library_c = ''
+
+for a, b in zip(library_a, library_b):
+    library_c += (a + b + ' ')
+
+print(library_c)
